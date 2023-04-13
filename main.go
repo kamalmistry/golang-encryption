92c450e0e0e56cc43d050af3fad8b3f6b93f7b8a
Kamal Mistry <tokamalmistry@gmail.com>
2023-04-13 19:06:06 +0530

    First working version with programmatically generated Keys

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/ProtonMail/gopenpgp/v2/crypto"
 	"github.com/ProtonMail/gopenpgp/v2/helper"
@@ -46,53 +47,40 @@ func generateKeys(password string) (string, string) {
 	}
 	// fmt.Println("privateKey string: ", privateKey)
 
-	// Curve25519, string
-	// ecKey, err := helper.GenerateKey(name, email, passphrase, "x25519", 0)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("ecKey: ", ecKey)
-
-	// RSA, Key struct
-	rsaKeyS, err := crypto.GenerateKey(name, email, "rsa", rsaBits)
+	keyRing, err := crypto.NewKeyFromArmoredReader(strings.NewReader(privateKey))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	// fmt.Println("rsaKey struct: ", rsaKeyS.GetEntity().PrivateKey)
-	pubKey, err := rsaKeyS.GetArmoredPublicKey()
+	publicKey, err := keyRing.GetArmoredPublicKey()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	// fmt.Println("pubKey: ", pubKey)
+	// fmt.Println("publicKey string: ", publicKey)
 
-	return privateKey, pubKey
-	// Curve25519, Key struct
-	// ecKeyS, err := crypto.GenerateKey(name, email, "x25519", 0)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("ecKeyS: ", ecKeyS)
+	return privateKey, publicKey
 }
+
 func main() {
-	fmt.Println("Hi start now ...")
-	const password = "LongSecret"
+	const password = "SomeLongSecret"
 	passphrase := []byte(password)
 	prikey, pubkey := generateKeys(password)
 
-	fmt.Println("priKey", prikey)
-	fmt.Println("pubkey", pubkey)
+	message := "this is pretty innocent plain text!!"
+	fmt.Println("Original message: ", message)
+	// fmt.Println("priKey", prikey)
+	// fmt.Println("pubkey", pubkey)
 
 	// encrypt plain text message using public key
-	armor, err := helper.EncryptMessageArmored(pubkey, "this is pretty innocent plain text!!")
+	armor, err := helper.EncryptMessageArmored(pubkey, message)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("armor: ", armor)
+	fmt.Println("encrypted text: \n", armor)
 
 	// decrypt armored encrypted message using the private key and obtain plain text
 	decrypted, err := helper.DecryptMessageArmored(prikey, passphrase, armor)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("decrypted: ", decrypted)
+	fmt.Println("decrypted text: \n", decrypted)
 }
